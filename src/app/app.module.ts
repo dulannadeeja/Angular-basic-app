a9c6062cd35bf9876a8199540f8f5f81da0ba62c
dulan nadeeja abeysinghe <dulnnadeeja@gmail.com>
2023-03-09 10:44:02 -0800

    first commit by me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
   ],
   imports: [
     BrowserModule
