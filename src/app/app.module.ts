9f917e22a19b02aa191a51fe38c9fbfeb1a49cc8
dulan nadeeja abeysinghe <dulnnadeeja@gmail.com>
2023-03-10 08:13:57 -0800

    bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
+    NavbarComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule
